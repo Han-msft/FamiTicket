a34a021c1cfb575413a60a05c8a54d092f716215
jimmy4834 <jimmy4834@hotmail.com>
2019-01-12 11:06:35 +0800

    Create auto_purchase.js

---
+++ auto_purchase.js
@@ -0,0 +1,47 @@
+(function() {
+//page 1
+var firstSubmit = submitData;
+submitData(window.location.search.substr(6), '');
+
+var page2_done = false;
+$(document).ajaxComplete(function() {
+	if (page2_done) return;
+	console.log('page 2 callback');
+	
+	$("#game_page").find('td').each(function(td, obj) {
+		if (obj.onclick) {
+			obj.onclick();
+			page2_done = true;
+			return false;
+		}
+	});
+
+	if (!page2_done) {
+		window.setTimeout (function() {
+			firstSubmit(window.location.search.substr(6), '');
+			console.log('retry');
+		}, 1000);
+		return;
+	}
+
+	var page3_done = false;
+	$(document).ajaxComplete(function() {
+		if (page3_done) return;
+		console.log('page 3 callback');
+		$("#game_page").find('tr').each(function(td, obj) {
+			if (obj.onclick) {
+				obj.onclick();
+				page3_done = true;
+				return false;
+			}
+		});
+
+		$(document).ajaxComplete(function() {
+			console.log('page 4 callback');
+			$("#game_page").find('select')[0].value = 1;
+			$("#game_page").find('select')[0].onchange();
+			//$("#game_page").find('a')[1].onclick();
+		});
+	});
+});
+})(); 
